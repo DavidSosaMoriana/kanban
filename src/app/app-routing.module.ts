df69929417f6ea71423923b9b0077f79cf17caee
DavidSosaMoriana <sosamorianadav@gmail.com>
2022-06-20 10:18:06 +0100

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { MainViewComponent } from './pages/main-view/main-view.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: MainViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
